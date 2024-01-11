f53ce0b4a3b5b9679c6fc8854f42a9e3151f6979
Dan-Pasqua <dlpasqua@comcast.net>
2024-01-11 16:15:49 -0500

    Refactored some code to use more helpers

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,13 @@
 /// <reference types='Cypress' />
 import '../utils/helpers';
-import { completeInformationForm, openCalendar } from '../utils/helpers';
+import {
+  bookTrip,
+  checkRoomInformationApproachOne,
+  checkRoomInformationApproachTwo,
+  completeInformationForm,
+  openCalendar,
+  selectDates,
+} from '../utils/helpers';
 const errors = require('../fixtures/error-messages.json');
 const months = require('../fixtures/months.json');
 const informationForm = require('../fixtures/information-form.json');
@@ -12,7 +19,7 @@ describe('Restful Booker Platform Demo', () => {
     cy.get('.hotel-logoUrl').should('exist').and('be.visible');
   });
 
-  it('displays room options on the homepage', () => {
+  it.only('displays room options on the homepage', () => {
     /* User Story #1 */
     /* ---------- APPROACH #1 ---------- */
     // Check for Rooms header
@@ -29,94 +36,25 @@ describe('Restful Booker Platform Demo', () => {
       .and('be.visible');
 
     // Check room information
-    cy.get('.hotel-room-info')
-      .first()
-      .should('exist')
-      .and('be.visible')
-      .within(() => {
-        cy.get('h3')
-          .should('exist')
-          .and('be.visible')
-          .and('have.text', 'single');
-
-        cy.get('p')
-          .should('exist')
-          .and('be.visible')
-          .and(
-            'have.text',
-            'Aenean porttitor mauris sit amet lacinia molestie. In posuere accumsan aliquet. Maecenas sit amet nisl massa. Interdum et malesuada fames ac ante.'
-          );
-
-        // Check room amenities
-        const amenities = ['TV', 'WiFi', 'Safe'];
-
-        for (let i = 0; i < amenities.length; i++) {
-          cy.get(`ul > :nth-child(${i + 1})`)
-            .should('exist')
-            .and('be.visible')
-            .and('have.text', amenities[i]);
-        }
-      });
+    cy.log('Approach #1');
+    checkRoomInformationApproachOne();
 
     /*
           In the above approach, I verified the first available room since only one room appeared on the 
-          site around 90 percent of the time I had it open. Since the availability included multiple rooms 
-          only on a few occasions, I added the approach below to demonstrate the ability to iterate 
-          through each room and print its information as if I were using a fixture file. Using a fixture 
-          file to verify availability based on the calendar bookings would require a cleanup script to reset 
-          the room availability after the test is finished running.
+          site around 90 percent of the time I had it open. 
+          
+          Since the availability included multiple rooms only on a few occasions, I added the approach 
+          below to demonstrate the ability to iterate through each room and print its information as if 
+          I were using a fixture file. Using a fixture file to verify availability based on the calendar 
+          bookings would require a cleanup script to reset the room availability after the test is finished running.
 
           If I had access to the database, I'd write SQL queries to return available rooms and their 
           information and compare it to what is visible in the UI.
     */
 
     /* ---------- APPROACH #2 ---------- */
-    cy.get('.hotel-room-info').then(($availableRooms) => {
-      const roomCount = $availableRooms.length;
-      let wheelchairAccessible = cy.get('.wheelchair');
-
-      for (let i = 0; i < roomCount; i++) {
-        cy.get(`:nth-child(${i + 1}) .hotel-room-info`).within(() => {
-          cy.get('h3').then(($room) => {
-            // Print room type
-            let roomType = $room.text();
-            cy.log(roomType);
-
-            // Print handicap accessible
-            !wheelchairAccessible
-              ? cy.log('Not an accessible room')
-              : cy.log('Room is accessible');
-
-            // Print room information
-            cy.get('p')
-              .should('exist')
-              .and('be.visible')
-              .then(($description) => {
-                let roomInformation = $description.text();
-                cy.log(roomInformation);
-              });
-
-            // Print amenities
-            cy.get('ul')
-              .should('exist')
-              .and('be.visible')
-              .then(($list) => {
-                let listLength = $list.children().length;
-
-                for (let x = 0; x < listLength; x++) {
-                  cy.get(`ul > :nth-child(${x + 1})`)
-                    .should('exist')
-                    .and('be.visible')
-                    .then(($amenity) => {
-                      let listItem = $amenity.text();
-                      cy.log(listItem);
-                    });
-                }
-              });
-          });
-        });
-      }
-    });
+    cy.log('Approach #2');
+    checkRoomInformationApproachTwo();
   });
 
   it('loads the availability calendar and contact information form after clicking on the Book this room button', () => {
@@ -193,8 +131,10 @@ describe('Restful Booker Platform Demo', () => {
       .click()
       .then(() => {
         // Check error messages
+
         /* ---------- BUG FOUND: Error messages are not in the same order from run to run ---------- */
         /* ---------- BUG FOUND: Error messages do not specify inputs, except for Firstname and Lastname ---------- */
+
         /* Fixture and related code saved to demonstrate preparations for future bug fix */
         cy.get('.alert-danger').should('exist').and('be.visible');
         // .within(() => {
@@ -208,11 +148,11 @@ describe('Restful Booker Platform Demo', () => {
       });
   });
 
-  it.skip('produces a "Booking successful" pop-up after a successful booking', () => {
+  it('produces a "Booking successful" pop-up after a successful booking', () => {
     openCalendar();
 
     // Select dates
-    let num = Math.floor(Math.random() * 7 + 3);
+    let num = Math.floor(Math.random() * 8 + 3);
     for (let i = 0; i < num; i++) {
       cy.get('button')
         .contains('Next')
@@ -222,48 +162,13 @@ describe('Restful Booker Platform Demo', () => {
         .click();
     }
 
-    let firstDay;
-    let lastDay;
-
-    cy.get('.rbc-date-cell:not(".rbc-off-range")')
-      .eq(1)
-      .drag('.rbc-date-cell:not(".rbc-off-range"):nth(15)');
+    selectDates();
 
     // Complete information form
     completeInformationForm();
 
-    // // Book trip
-    // cy.get('button')
-    //   .contains('Book')
-    //   .should('exist')
-    //   .and('be.visible')
-    //   .click()
-    //   .then(() => {
-    //     cy.get('.confirmation-modal')
-    //       .should('exist')
-    //       .and('be.visible')
-    //       .within(() => {
-    //         // Check contents
-    //         cy.get('h3')
-    //           .should('exist')
-    //           .and('be.visible')
-    //           .and('have.text', 'Booking Successful!');
-
-    //         // Check dates
-    //         cy.get('p')
-    //           .should('exist')
-    //           .and('be.visible')
-    //           .and(
-    //             'have.text',
-    //             'Congratulations! Your booking has been confirmed for:'
-    //           );
-
-    //         cy.get('.btn-outline-primary')
-    //           .should('exist')
-    //           .and('be.visible')
-    //           .and('have.text', 'Close')
-    //           .click();
-    //       });
-    //   });
+    // Book trip
+    /* ---------- BUG FOUND: Minimum of two nights not enforced ---------- */
+    bookTrip();
   });
 });
--- cypress/utils/helpers.js
+++ cypress/utils/helpers.js
@@ -2,6 +2,90 @@
 
 const informationForm = require('../fixtures/information-form.json');
 
+export function checkRoomInformationApproachOne() {
+  cy.get('.hotel-room-info')
+    .first()
+    .should('exist')
+    .and('be.visible')
+    .within(() => {
+      cy.get('h3').should('exist').and('be.visible').and('have.text', 'single');
+
+      // Print handicap accessible
+      let wheelchairAccessible = cy.get('.wheelchair');
+
+      !wheelchairAccessible
+        ? cy.log('Not an accessible room')
+        : cy.log('Room is accessible');
+
+      cy.get('p')
+        .should('exist')
+        .and('be.visible')
+        .and(
+          'have.text',
+          'Aenean porttitor mauris sit amet lacinia molestie. In posuere accumsan aliquet. Maecenas sit amet nisl massa. Interdum et malesuada fames ac ante.'
+        );
+
+      // Check room amenities
+      const amenities = ['TV', 'WiFi', 'Safe'];
+
+      for (let i = 0; i < amenities.length; i++) {
+        cy.get(`ul > :nth-child(${i + 1})`)
+          .should('exist')
+          .and('be.visible')
+          .and('have.text', amenities[i]);
+      }
+    });
+}
+
+export function checkRoomInformationApproachTwo() {
+  cy.get('.hotel-room-info').then(($availableRooms) => {
+    const roomCount = $availableRooms.length;
+    let wheelchairAccessible = cy.get('.wheelchair');
+
+    for (let i = 0; i < roomCount; i++) {
+      cy.get(`:nth-child(${i + 1}) .hotel-room-info`).within(() => {
+        cy.get('h3').then(($room) => {
+          // Print room type
+          let roomType = $room.text();
+          cy.log(roomType);
+
+          // Print handicap accessible
+          !wheelchairAccessible
+            ? cy.log('Not an accessible room')
+            : cy.log('Room is accessible');
+
+          // Print room information
+          cy.get('p')
+            .should('exist')
+            .and('be.visible')
+            .then(($description) => {
+              let roomInformation = $description.text();
+              cy.log(roomInformation);
+            });
+
+          // Print amenities
+          cy.get('ul')
+            .should('exist')
+            .and('be.visible')
+            .then(($list) => {
+              let listLength = $list.children().length;
+
+              for (let x = 0; x < listLength; x++) {
+                cy.get(`ul > :nth-child(${x + 1})`)
+                  .should('exist')
+                  .and('be.visible')
+                  .then(($amenity) => {
+                    let listItem = $amenity.text();
+                    cy.log(listItem);
+                  });
+              }
+            });
+        });
+      });
+    }
+  });
+}
+
 export function openCalendar() {
   cy.intercept('GET', 'https://automationintesting.online/report/room/1').as(
     'openCalendar'
@@ -15,6 +99,12 @@ export function openCalendar() {
   cy.wait('@openCalendar');
 }
 
+export function selectDates() {
+  cy.get('.rbc-date-cell:not(".rbc-off-range")')
+    .eq(1)
+    .drag('.rbc-date-cell:not(".rbc-off-range"):nth(15)');
+}
+
 export function completeInformationForm() {
   informationForm.forEach((input) => {
     cy.get(input.input)
@@ -24,4 +114,39 @@ export function completeInformationForm() {
         cy.get($field).type(input.text);
       });
   });
+}
+
+export function bookTrip() {
+  cy.get('button')
+    .contains('Book')
+    .should('exist')
+    .and('be.visible')
+    .click()
+    .then(() => {
+      cy.get('.confirmation-modal')
+        .should('exist')
+        .and('be.visible')
+        .within(() => {
+          // Check contents
+          cy.get('h3')
+            .should('exist')
+            .and('be.visible')
+            .and('have.text', 'Booking Successful!');
+
+          // Check dates
+          cy.get('p')
+            .should('exist')
+            .and('be.visible')
+            .and(
+              'contain.text',
+              'Congratulations! Your booking has been confirmed for:'
+            );
+
+          cy.get('.btn-outline-primary')
+            .should('exist')
+            .and('be.visible')
+            .and('have.text', 'Close')
+            .click();
+        });
+    });
 }
